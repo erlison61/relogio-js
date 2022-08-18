eb8df6812de84e05ef1ef3032201ff8a299b7bcb
erlison61 <99809349+erlison61@users.noreply.github.com>
2022-08-18 09:32:51 -0300

    Update relogio.js

--- relogio.js
+++ relogio.js
@@ -1,15 +1,9 @@
 function relogio(){
-    var dia = new Date()
-    var hour = dia.getHours()
-    var min = dia.getMinutes()
-    var sec = dia.getSeconds()
-    if(hour < 10){
-        hour = 0+hour
-    }else if( min < 10){
-        min = 0+min
-    }else if( sec < 10){
-        sec = 0+sec
-    }
-    document.getElementById('relogio').innerHTML= hour+':'+min+':'+sec
+   const newDate=document.getElementById('relogio')
+   const hour= new Date().toLocaleTimeString('en-us')
+   newDate.innerHTML=hour
 }
+
+
+
 setInterval('relogio()', 1000)
